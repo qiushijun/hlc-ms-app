7e6398e3bf6e9758e0820ecd18e641a5b77f308f
hlooc <hloocs@aliyun.com>
2018-06-09 11:04:28 +0800

    增加服务注册监控

--- registry/src/main/java/com/hlooc/registry/config/WebSecurityConfig.java
+++ registry/src/main/java/com/hlooc/registry/config/WebSecurityConfig.java
@@ -1,17 +1,32 @@
 package com.hlooc.registry.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll()
+//                .and().csrf().disable();
 
-        http.csrf().disable();//关闭csrf
-        super.configure(http);
+//        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+//        successHandler.setTargetUrlParameter("redirectTo");
+//
+//        http.authorizeRequests()
+//                .antMatchers("/assets/**").permitAll()
+//                .antMatchers( "/actuator/**").permitAll()
+//                .antMatchers( "/login").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().loginPage( "/login").successHandler(successHandler).and()
+//                .logout().logoutUrl("/logout").and()
+//                .httpBasic().and()
+//                .csrf().disable();
+
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
     }
-
 }
